8fbb17cedfb5f9aaf398299dc984f053ca83e7d8
Steven Ning <60117716+stevenning203@users.noreply.github.com>
2020-07-06 17:51:34 -0600

    Add files via upload

---
+++ Items/NPCBOSS/MaraProjectileOrb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace MoreWeaponaria.Items.NPCBOSS
+{
+    public class MaraProjectileOrb : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("ORB!");
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 30;
+            projectile.height = 30;
+            projectile.aiStyle = 1;
+            aiType = ProjectileID.Bullet;
+            projectile.friendly = false;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = false;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 2000;
+            projectile.hostile = true;
+            projectile.scale = 1f;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.Yellow;
+        }
+        public override void AI()
+        {
+            projectile.velocity.Y += projectile.ai[0];
+            projectile.rotation += 0.25f;
+            if (projectile.rotation >= 6)
+            {
+                projectile.rotation = 0f;
+            }
+        }
+    }
+}
---
+++ Items/Weapons/AncientRingOfMara.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MoreWeaponaria.Items.Weapons
+{
+    public class AncientRingOfMara : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Ancient Ring of Mara");
+            Tooltip.SetDefault("Increases your max number of minions by 5\nIncreases maximum mana by 100");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 32;
+            item.value = 10000000;
+            item.rare = ItemRarityID.Red;
+            item.accessory = true;
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.maxMinions += 5;
+            player.statManaMax2 += 100;
+        }
+    }
+}
